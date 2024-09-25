5158a0d9569528d6d24d8c6c2b06a9138bd2d6c5
Gabriel Bisson--Barret <gabriel.bisson--barret@mmibordeaux.com>
2024-09-25 17:00:14 +0200

    undo test

---
+++ ile.js
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+return (
+    <main>
+    <Spline
+        scene="https://prod.spline.design/k9WE91fRhmGQW6O4/scene.splinecode" 
+    />
+    </main>
+);
+}
